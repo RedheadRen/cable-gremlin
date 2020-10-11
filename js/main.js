f6cc274e3eeb2de8449e1c5a76ab32e5b20a967b
renny_fve <rennyfve@gmail.com>
2020-10-10 18:02:22 -0700

    Changed the hamburger menu setup

--- js/main.js
+++ js/main.js
@@ -1,24 +1,6 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-    
-    burger.addEventListener('click', () => {
-        //Toggle Nav
-        nav.classList.toggle('nav-active');
+document.querySelector('.hamburger-menu')
+.addEventListener('click', ()=>{
+    document.querySelector('.navigation')
+    .classList.toggle('change');
+});
 
-        //Animate Links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation ='';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.3}s`;
-            }
-        });
-        //Burger Animation
-        burger.classList.toggle('toggle');
-    });
-
-}  
-
-navSlide();
